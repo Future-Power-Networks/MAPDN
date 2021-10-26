f0c30abe2cfd484e72c53bcfb9819ab757cfbad1
hsvgbkhgbv <wangjianhong1993@gmail.com>
2021-10-26 16:01:54 +0100

    clean up

--- environments/var_voltage_control/voltage_loss/courant_beltrami.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-
-def courant_beltrami_loss(vs, v_lower, v_upper):
-    def _courant_beltrami_loss(v):
-        return np.square(max(0, v - v_upper)) + np.square(max(0, v_lower - v))
-    return np.array([_courant_beltrami_loss(v) for v in vs])
